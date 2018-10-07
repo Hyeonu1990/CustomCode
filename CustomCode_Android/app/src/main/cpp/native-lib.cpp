44a0a34e2898c5292b0e3cd13e3680abe2c96450
Hyeonu Choi <renewal1990@gmail.com>
2018-10-07 21:59:31 +0900

    안드로이드 버전 추가

---
+++ CustomCode_Android/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,30 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include "CustomCodeRecognition.h"
+using namespace cv;
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_customcode_customcode_1android_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_customcode_customcode_1android_MainActivity_CustomCode(JNIEnv *env, jobject instance,
+                                                            jlong matAddrInput,
+                                                            jlong matAddrResult) {
+
+    // TODO
+    Mat &matInput = *(Mat *)matAddrInput;
+    Mat &matResult = *(Mat *)matAddrResult;
+    cvtColor(matInput, matResult, CV_RGBA2GRAY);
+
+    string result = "";
+    CustomCode customcode;
+    vector<Point2f> markers;
+    customcode.recognition(&matInput, &markers, &result);
+}
